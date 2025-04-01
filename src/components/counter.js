b4c97535252d7ba8b5094e8ed05612e3e0a94e01
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-01 11:41:45 +0300

    adding buttons and divs

---
+++ src/components/counter.js
@@ -0,0 +1,13 @@
+const CounterApp = () => {
+    return(
+        <div>
+            <p>{counter}</p>
+            <div className="buttons">
+                <button>+</button>
+                <button>-</button>
+            </div>
+        </div>
+    )
+}
+
+export default CounterApp
